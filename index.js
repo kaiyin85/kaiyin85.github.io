f5747eeb3c472e8c3d18ce9eccdda38210261bc5
kaiyin85 <kaiyin85@gmail.com>
2016-03-16 19:53:51 -0400

    First

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function my_first_js_function() {
+    var taco = 1;
+    var t = 'srt';
+
+    if (k == 12) {
+        return 42
+    }
+
+    for (var i = 0; i < 10; i++) {
+
+    }
+
+    return 42;
+}
+
+function kosbie_face_click_handler() {
+    alert("ouch");
+}
+
+
+function on_page_ready(){
+    $(".kosbiesface").click(kosbie_face_click_handler)   
+}
+
+$(document).ready(on_page_ready)
